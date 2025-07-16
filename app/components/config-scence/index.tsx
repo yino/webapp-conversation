fea56069cd928d609160a2c3c012d753725c5790
PL MM <plmm@PLdeMacBook-Air.local>
2025-07-16 10:55:26 +0800

    更改高度

--- app/components/config-scence/index.tsx
+++ app/components/config-scence/index.tsx
@@ -5,7 +5,7 @@ import Welcome from '../welcome'
 
 const ConfigSence: FC<IWelcomeProps> = (props) => {
   return (
-    <div className='mb-5 antialiased font-sans overflow-hidden shrink-0'>
+    <div className=' antialiased font-sans overflow-hidden shrink-0'>
       <Welcome {...props} />
     </div>
   )
