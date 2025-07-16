5706315b5995a762978069620bc5097f75a345e8
PL MM <plmm@PLdeMacBook-Air.local>
2025-07-16 10:29:44 +0800

    修复输入框放大问题

--- app/components/chat/index.tsx
+++ app/components/chat/index.tsx
@@ -257,17 +257,20 @@ const Chat: FC<IChatProps> = ({
                 </div>
               </>
             )}
-            <Textarea
-              className={`
-              block w-full px-2 pr-[118px] py-[7px] leading-5 max-h-none text-sm text-gray-700 outline-none appearance-none resize-none
-              ${visionConfig?.enabled && 'pl-12'}
-            `}
-              value={query}
-              onChange={handleContentChange}
-              onKeyUp={handleKeyUp}
-              onKeyDown={handleKeyDown}
-              autoSize
-            />
+           <Textarea
+             className={cn(
+               'block w-full px-2 pr-[118px] py-[7px] leading-5 max-h-none text-sm text-gray-700 outline-none appearance-none resize-none',
+               visionConfig?.enabled && 'pl-12'
+             )}
+             style={{
+               WebkitTextSizeAdjust: '100%', 
+             }}
+             value={query}
+             onChange={handleContentChange}
+             onKeyUp={handleKeyUp}
+             onKeyDown={handleKeyDown}
+             autoSize
+           />
             <div className="absolute bottom-2 right-2 flex items-center h-8">
 
               <Tooltip
